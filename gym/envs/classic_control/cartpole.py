65515777ad4cf26403eed21f0c0380843d82e50c
Shreenabh Agrawal <me@shreenabh.com>
2022-07-21 15:15:08 +0530

    FM = 10 [TT-90, A-0.05, XT-4.8]
    FM - Force Magnitude (N)
    TT - Theta Threshold (deg)
    A - Initial Angle (rad)
    XT - X Threshold

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -93,12 +93,12 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         self.total_mass = self.masspole + self.masscart
         self.length = 0.5  # actually half the pole's length
         self.polemass_length = self.masspole * self.length
-        self.force_mag = 20.0
+        self.force_mag = 10.0
         self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = "euler"
 
         # Angle at which to fail the episode
-        self.theta_threshold_radians = 12 * 2 * math.pi / 360
+        self.theta_threshold_radians = 90 * 2 * math.pi / 360
         self.x_threshold = 4.8
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation
