e121f1bc185d450a7f1432436e3039c95a388a97
Shreenabh Agrawal <me@shreenabh.com>
2022-07-21 14:26:22 +0530

    FM = 20 [TT-90, A-1, XT- 24]

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -93,13 +93,13 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         self.total_mass = self.masspole + self.masscart
         self.length = 0.5  # actually half the pole's length
         self.polemass_length = self.masspole * self.length
-        self.force_mag = 33.0
+        self.force_mag = 20.0
         self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = "euler"
 
         # Angle at which to fail the episode
         self.theta_threshold_radians = 90 * 2 * math.pi / 360
-        self.x_threshold = 2.4
+        self.x_threshold = 24
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation
         # is still within bounds.
@@ -215,7 +215,7 @@ class CartPoleEnv(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         #self.state = self.np_random.uniform(low=low, high=high, size=(4,))
         state2 = self.np_random.uniform(low=0.0, high=0.0, size=(1,))
         #np.random.seed(2)
-        state3 = self.np_random.uniform(low=-0.0349066, high=0.0349066, size=(1,)) #2 deg
+        state3 = self.np_random.uniform(low=-1, high=1, size=(1,)) #1 rad
         state4 = self.np_random.uniform(low=0.0, high=0.0, size=(1,))
         state1 = self.np_random.uniform(low=0.0, high=0.0, size=(1,))
         self.state=np.concatenate((state1, state2, state3, state4))
